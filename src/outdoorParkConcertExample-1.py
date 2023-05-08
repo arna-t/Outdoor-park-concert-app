8bd481b377b3b3c31709db985866a5da44b0c998
Arna Togayeva <arna.togayeva@gmail.com>
2023-05-07 22:19:46 -0700

    created a menu fuction

--- src/outdoorParkConcertExample-1.py
+++ src/outdoorParkConcertExample-1.py
@@ -3,6 +3,35 @@ This example code creates a 2d list (2d matrix) that can store seating.
 The matrix is populated with . since all seats are available
 """
 
+def menu():
+    print("---------------------------------------------------------")
+    print("          Welcome to the Outdoor Park Concert App!       ")
+    print("---------------------------------------------------------")
+    print()
+    print("[v] view seating")
+    print("[b] buy tickets")
+    print("[s] search for a customer name and display the tickets purchased")
+    print("[d] display all the pirchase made and total amount of income")
+    print("[q] to quit")
+    print()
+    userInput = input("Enter a command: ")
+    lowerInput = userInput.lower()
+    firstChar = lowerInput[0:1]
+
+    userQuit = False
+    while (not userQuit):
+
+		#calling the menu function
+	    menu()
+userInput = input("Enter a command: ")
+lowerInput = userInput.lower()
+firstChar = lowerInput[0:1]
+
+if firstChar == 'v':
+     
+
+
+
 # our test matrix has 4 rows and 10 columns
 n_row = 4
 n_col = 10
