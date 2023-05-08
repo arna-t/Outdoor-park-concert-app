1b858bfd93b6a3c596e4c0bf747c770ec6253c8c
Arna Togayeva <arna.togayeva@gmail.com>
2023-05-08 00:30:14 -0700

    created functions: buyTickets, printReceipt, calculateCost

--- src/outdoorParkConcertExample-1.py
+++ src/outdoorParkConcertExample-1.py
@@ -75,6 +75,34 @@ def printSeating():
 def buyTickets():
     n_tix = input("Number of seats to buy: ")
     seat_start = input("Starting seat (ex. 3d): ")
+    print("f{n_tix} seats starting at {seat_start} are available for purchase.")
+    name = input("Enter your name: ")
+    email = input("Enter your email address: ")
+    printReceipt()
+
+def calculateCost(n_tix):
+    cost = n_tix * 20
+    mask_fee = n_tix * 5
+    sub_total = cost + mask_fee
+    tax = sub_total * 0.0725
+    total = sub_total + tax
+
+def printReceipt():
+    print("------------------------------------")
+    print("            Receipt                 ")
+    print("------------------------------------")
+    print("Name:   {name}")
+    print("Email: {email}")
+    print("Number of tickets: {n_tix}")
+  #  print("Seats: {seat_start}")
+    print("Ticket Cost: {calculateCost.cost}")
+    print("Mask fee: {calculateCost.mask_fee} ")
+    print("Subtotal: {calculateCost.sub_total}")
+    print("Tax: {calculateCost.tax}")
+    print("--------------------------------------")
+    print("Total  {calculateCost.total}")
+
+
 
 # create some available seating
 """""
