731ac54ca50884cc1fd8d14d23ce2554286c2259
Arna Togayeva <arna.togayeva@gmail.com>
2023-05-08 00:09:21 -0700

    created while loop with if elif statements

--- src/outdoorParkConcertExample-1.py
+++ src/outdoorParkConcertExample-1.py
@@ -14,32 +14,70 @@ def menu():
     print("[d] display all the pirchase made and total amount of income")
     print("[q] to quit")
     print()
+    
+
+userQuit = False
+while (not userQuit):
+
+		#calling the menu function
+    menu()
     userInput = input("Enter a command: ")
     lowerInput = userInput.lower()
     firstChar = lowerInput[0:1]
 
-    userQuit = False
-    while (not userQuit):
+    if firstChar == 'v':
+     # call function printSeating()
+        printSeating()
+     # our test matrix has 4 rows and 10 columns
+        n_row = 4
+        n_col = 10
 
-		#calling the menu function
-	    menu()
-userInput = input("Enter a command: ")
-lowerInput = userInput.lower()
-firstChar = lowerInput[0:1]
+    # available seat
+        available_seat = '.'
+     # create some available seating
 
-if firstChar == 'v':
+        seating = []
+        for r in range(n_row):
+            row = []
+            for c in range(n_col):
+                row.append(available_seat)
+        seating.append(row)
+
+# print available seating
+        for r in range(n_row):
+            print(r+1, end="\t")
+            for c in range(n_col):
+                print(seating[r][c], end=" ")
+        print()
+
+   # elif firstChar == 'b':
+     # call function buyTickets()
+    elif firstChar == 'q':
+        userQuit = True
+    else:
+        print("ERROR: " + firstChar + " is not a valid command")
+
+print("\n")
+print("Thank you for using the Outdoor Park Concert App!")
+print("\n")
      
+#def createSeating():
 
 
+def printSeating():
+    print("------------------------------------------")
+    print("                Seating                   ")
+    print("------------------------------------------")
+    print("    a b c d e f g h i j k l m n o p      type     price ")
+    print("------------------------------------    ------   ------- ")
 
-# our test matrix has 4 rows and 10 columns
-n_row = 4
-n_col = 10
 
-# available seat
-available_seat = '.'
+def buyTickets():
+    n_tix = input("Number of seats to buy: ")
+    seat_start = input("Starting seat (ex. 3d): ")
 
 # create some available seating
+"""""
 seating = []
 for r in range(n_row):
     row = []
@@ -53,3 +91,5 @@ for r in range(n_row):
     for c in range(n_col):
         print(seating[r][c], end=" ")
     print()
+
+"""
