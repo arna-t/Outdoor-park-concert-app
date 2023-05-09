0f2d25d8387346e1e35ca24d4b57098fd8875a47
Arna Togayeva <arna.togayeva@gmail.com>
2023-05-08 22:37:22 -0700

    modified printSeating to properly print the seating chart

--- src/outdoorParkConcertExample-1.py
+++ src/outdoorParkConcertExample-1.py
@@ -2,6 +2,11 @@
 This example code creates a 2d list (2d matrix) that can store seating.
 The matrix is populated with . since all seats are available
 """
+n_row = 4
+n_col = 10
+
+
+# create some available seating
 
 def menu():
     print("---------------------------------------------------------")
@@ -14,9 +19,76 @@ def menu():
     print("[d] display all the pirchase made and total amount of income")
     print("[q] to quit")
     print()
-    
 
+
+def createSeating():
+    # available seat
+    available_seat = '.'
+    seating = []
+    for r in range(n_row):
+        row = []
+        for c in range(n_col):
+            row.append(available_seat)
+        seating.append(row)
+    return seating
+
+def printSeating(seating):
+    print("-----------------------------------------------------------")
+    print("                        Seating                            ")
+    print("-----------------------------------------------------------")
+    #print("a b c d e f g h i j      type     price ")
+   # print('        ', end='')
+    print ("\t", end="")
+    for i in range(0,10):
+        print ("%s " % chr(ord('a')+i), end = "   ")
+    print("")
+   
+ 
+# print available seating
+    for r in range(n_row):
+        print(r+1, end = "\t")
+        for c in range(n_col):
+            print(seating[r][c], end="    ")
+        print()  
+
+
+#def printReceipt(name, email):
+   
+
+
+def buyTickets(seating):
+    n_tix = int(input("Number of seats to buy: "))
+    #seat_start = input("Starting seat (ex. 3d): ")
+    seat_selected = input("Select seat: ")
+    r = seat_selected[0:1]
+    print(f"{n_tix} seats starting at {seat_selected} are available for purchase.")
+    name = input("Enter your name: ")
+    email = input("Enter your email address: ")
+    print("------------------------------------")
+    print("            Receipt                 ")
+    print("------------------------------------")
+   # def calculateCost(n_tix):
+    cost = n_tix * 20
+    mask_fee = n_tix * 5
+    sub_total = cost + mask_fee
+    tax = float(sub_total) * 0.0725
+    total = float(sub_total) + float(tax)
+    print(f"Name:  {name}")
+    print(f"Email: {email}")
+    print(f"Number of tickets: {n_tix}")
+    #print("Seats: {seat_start}")
+    print(f"Ticket Cost: ${cost}")
+    print(f"Mask fee: ${mask_fee} ")
+    print(f"Subtotal: ${sub_total}")
+    print("Tax: $", round(tax, 2))
+    print("--------------------------------------")
+    print("Total: $", round(total, 2))
+    menu()
+
+seating = createSeating()
 userQuit = False
+seating[3][5] = 'G'
+
 while (not userQuit):
 
 		#calling the menu function
@@ -27,31 +99,11 @@ while (not userQuit):
 
     if firstChar == 'v':
      # call function printSeating()
-        printSeating()
-     # our test matrix has 4 rows and 10 columns
-        n_row = 4
-        n_col = 10
+        printSeating(seating)
+     # our test matrix has 4 rows and 10 columns    
 
-    # available seat
-        available_seat = '.'
-     # create some available seating
-
-        seating = []
-        for r in range(n_row):
-            row = []
-            for c in range(n_col):
-                row.append(available_seat)
-        seating.append(row)
-
-# print available seating
-        for r in range(n_row):
-            print(r+1, end="\t")
-            for c in range(n_col):
-                print(seating[r][c], end=" ")
-        print()
-
-   # elif firstChar == 'b':
-     # call function buyTickets()
+    elif firstChar == 'b':
+        buyTickets(seating)
     elif firstChar == 'q':
         userQuit = True
     else:
@@ -61,63 +113,3 @@ print("\n")
 print("Thank you for using the Outdoor Park Concert App!")
 print("\n")
      
-#def createSeating():
-
-
-def printSeating():
-    print("------------------------------------------")
-    print("                Seating                   ")
-    print("------------------------------------------")
-    print("    a b c d e f g h i j k l m n o p      type     price ")
-    print("------------------------------------    ------   ------- ")
-
-
-def buyTickets():
-    n_tix = input("Number of seats to buy: ")
-    seat_start = input("Starting seat (ex. 3d): ")
-    print("f{n_tix} seats starting at {seat_start} are available for purchase.")
-    name = input("Enter your name: ")
-    email = input("Enter your email address: ")
-    printReceipt()
-
-def calculateCost(n_tix):
-    cost = n_tix * 20
-    mask_fee = n_tix * 5
-    sub_total = cost + mask_fee
-    tax = sub_total * 0.0725
-    total = sub_total + tax
-
-def printReceipt():
-    print("------------------------------------")
-    print("            Receipt                 ")
-    print("------------------------------------")
-    print("Name:   {name}")
-    print("Email: {email}")
-    print("Number of tickets: {n_tix}")
-  #  print("Seats: {seat_start}")
-    print("Ticket Cost: {calculateCost.cost}")
-    print("Mask fee: {calculateCost.mask_fee} ")
-    print("Subtotal: {calculateCost.sub_total}")
-    print("Tax: {calculateCost.tax}")
-    print("--------------------------------------")
-    print("Total  {calculateCost.total}")
-
-
-
-# create some available seating
-"""""
-seating = []
-for r in range(n_row):
-    row = []
-    for c in range(n_col):
-        row.append(available_seat)
-    seating.append(row)
-
-# print available seating
-for r in range(n_row):
-    print(r+1, end="\t")
-    for c in range(n_col):
-        print(seating[r][c], end=" ")
-    print()
-
-"""
